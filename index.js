64cbe9cabe928456cbc439f7509dd08793907181
Jesse Brown <Brownjesse8895@yahoo.com>
2021-05-22 18:06:42 -0500

    commit tree 48912ac1bcdb35e370a164daa1efb3eee186bf8a

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+  else if(distance > 2500){
+    return 'No can do.'
+  }
+  else{ 
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  const city = "NYC"
+  let result 
+  result = (destination === city) ? "Ok, sounds good." : "No go."
+  return result
+   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someTip){
+  let goodBye 
+  switch(someTip){
+    case 'generous':
+    goodBye  = "Thank you so much."
+    break;
+    case 'not as generous':
+      goodBye = "Thank you."
+      break;
+    case 'thank you for everything':
+      case 'thanks for everything':
+        goodBye = 'Bye.'
+      break;
+  }
+  return goodBye
 }
